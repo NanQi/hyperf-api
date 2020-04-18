8bdccb785199e6e589b97256d6b8b3d2db60580b
wzq <1309731083@qq.com>
2020-04-18 13:54:26 +0800

    test

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -9,18 +9,15 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
  */
-use App\Exception\Handler\ValidationExceptionHandler;
 use App\Exception\Handler\AppExceptionHandler;
 use App\Exception\Handler\BusinessExceptionHandler;
 use App\Exception\Handler\ValidationExceptionHandler;
-use App\Exception\Handler\JwtExceptionHandler;
 return [
     'handler' => [
         'http' => [
             ValidationExceptionHandler::class,
             BusinessExceptionHandler::class,
             AppExceptionHandler::class,
-            JwtExceptionHandler::class,
         ],
     ],
 ];
